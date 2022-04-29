2f88fef9134b723ffc992a8d121bf869c5c29120
Neville Ingram <nlingram@wpi.edu>
2022-04-29 12:17:41 -0400

    Working on imputation methods

--- models/random_forests.py
+++ models/random_forests.py
@@ -12,7 +12,7 @@ from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 from sklearn.preprocessing import LabelEncoder, MaxAbsScaler, MinMaxScaler, RobustScaler, StandardScaler
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.feature_selection import SelectKBest, SelectFpr, SelectFdr, SelectPercentile, SelectFwe, VarianceThreshold, SelectFromModel
-
+pd.set_option('display.max_columns', 25)
 
 x_train = pd.read_csv('./data/train.csv')
 x_test = pd.read_csv('./data/test.csv')
@@ -53,34 +53,45 @@ for i in categorical_features:
         x_train[i] = encoder.fit_transform(x_train[i])
         x_test[i] = encoder.fit_transform(x_test[i])
 
+for i in float_features:
+    x_train[i] = x_train[i].fillna(0)
+    x_test[i] = x_test[i].fillna(0)
+
 x_train["total_spent"] = x_train["RoomService"] + \
     x_train["FoodCourt"] + x_train["ShoppingMall"] + \
     x_train["Spa"] + x_train["VRDeck"]
 
-x_train["interactionAmentities1"] = x_train["FoodCourt"] * \
-    x_train["ShoppingMall"]
-x_train["interactionAmentities2"] = x_train["FoodCourt"] * \
-    x_train["RoomService"]
-x_train["interactionAmentities3"] = x_train["FoodCourt"] * \
-    x_train["Spa"]
-x_train["interactionAmentities4"] = x_train["FoodCourt"] * \
-    x_train["VRDeck"]
+x_test["total_spent"] = x_test["RoomService"] + \
+    x_test["FoodCourt"] + x_test["ShoppingMall"] + \
+    x_test["Spa"] + x_test["VRDeck"]
+
+# x_train["interactionAmentities1"] = x_train["FoodCourt"] * \
+#     x_train["ShoppingMall"]
+# x_train["interactionAmentities2"] = x_train["FoodCourt"] * \
+#     x_train["RoomService"]
+# x_train["interactionAmentities3"] = x_train["FoodCourt"] * \
+#     x_train["Spa"]
+# x_train["interactionAmentities4"] = x_train["FoodCourt"] * \
+#     x_train["VRDeck"]
+
+# x_test["interactionAmentities1"] = x_test["FoodCourt"] * \
+#     x_test["ShoppingMall"]
+# x_test["interactionAmentities2"] = x_test["FoodCourt"] * \
+#     x_test["RoomService"]
+# x_test["interactionAmentities3"] = x_test["FoodCourt"] * \
+#     x_test["Spa"]
+# x_test["interactionAmentities4"] = x_test["FoodCourt"] * \
+#     x_test["VRDeck"]
 
 
 # deck/num/side -> Side = P or S
 # B/0/P
-x_train["Cabin"] = x_train["Cabin"].str.replace('/', '')
-x_train["deck"] = x_train["Cabin"].str[0:1]
-x_train["num"] = x_train["Cabin"].str[1:2]
-x_train["side"] = x_train["Cabin"].str[2:]
-x_train["side"] = x_train["side"].str.replace('0', '')
+x_train[['deck', 'num', 'side']] = x_train['Cabin'].str.split('/', expand=True)
+
 x_train = x_train.drop(columns=["Cabin"])
 
-x_test["Cabin"] = x_test["Cabin"].str.replace('/', '')
-x_test["deck"] = x_test["Cabin"].str[0:1]
-x_test["num"] = x_test["Cabin"].str[1:2]
-x_test["side"] = x_test["Cabin"].str[2:]
-x_test["side"] = x_test["side"].str.replace('0', '')
+x_test[['deck', 'num', 'side']] = x_test['Cabin'].str.split('/', expand=True)
+
 x_test = x_test.drop(columns=["Cabin"])
 
 deckEncoder = LabelEncoder()
@@ -95,25 +106,18 @@ portEncoder = LabelEncoder()
 x_train["side"] = deckEncoder.fit_transform(x_train["side"])
 x_test["side"] = deckEncoder.fit_transform(x_test["side"])
 
-x_test["total_spent"] = x_test["RoomService"] + \
-    x_test["FoodCourt"] + x_test["ShoppingMall"] + \
-    x_test["Spa"] + x_test["VRDeck"]
-
-x_test["interactionAmentities1"] = x_test["FoodCourt"] * \
-    x_test["ShoppingMall"]
-x_test["interactionAmentities2"] = x_test["FoodCourt"] * \
-    x_test["RoomService"]
-x_test["interactionAmentities3"] = x_test["FoodCourt"] * \
-    x_test["Spa"]
-x_test["interactionAmentities4"] = x_test["FoodCourt"] * \
-    x_test["VRDeck"]
-
 feature_names = x_train.columns
 print("Number of features: ", len(feature_names))
 
-imputer = SimpleImputer()
-x_train = imputer.fit_transform(x_train)
-x_test = imputer.fit_transform(x_test)
+x_train['total_spent'] = x_train.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['total_spent'],
+    axis=1
+)
+
+x_test['total_spent'] = x_test.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['total_spent'],
+    axis=1
+)
 
 x_train = pd.DataFrame(data=x_train, columns=feature_names)
 x_test = pd.DataFrame(data=x_test, columns=feature_names)
