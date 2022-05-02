a4e2b6239904313755f395613ae9948b0994a77d
Neville Ingram <nlingram@wpi.edu>
2022-05-02 16:17:39 -0400

    First neural network attempt

--- models/nn.py
+++ models/nn.py
@@ -11,7 +11,16 @@ from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
 from sklearn.preprocessing import LabelEncoder, MaxAbsScaler, MinMaxScaler, OneHotEncoder, RobustScaler, StandardScaler
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.feature_selection import SelectKBest, SelectFpr, SelectFdr, SelectPercentile, SelectFwe, VarianceThreshold, SelectFromModel
+import keras as keras
+import keras.layers as layers
 pd.set_option('display.max_columns', 25)
+# Setup plotting
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
 
 x_train = pd.read_csv('./data/train.csv')
 x_test = pd.read_csv('./data/test.csv')
@@ -94,10 +103,7 @@ for i in categorical_features:
 feature_names = x_train.columns
 
 X_train, X_test, Y_train, Y_test = train_test_split(
-    x_train, y_train.values.ravel(), test_size=0.3, random_state=42)
-
-model = RandomForestClassifier(
-    max_depth=6, n_estimators=750, n_jobs=-1, random_state=42)
+    x_train, y_train, test_size=0.3, random_state=42)
 
 selector = SelectKBest(k=6, score_func=f_classif)
 # selector = PCA(0.99, random_state=42)
@@ -107,13 +113,51 @@ X_train = selector.transform(X_train)
 X_test = selector.transform(X_test)
 x_test = selector.transform(x_test)
 
-model.fit(X_train, Y_train)
+print(X_train.shape)
+print(X_test.shape)
+
+input_shape = X_train.shape[0]
+
+model = keras.Sequential([
+    layers.BatchNormalization(),
+    layers.Dense(units=1024, input_shape=[input_shape], activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dense(units=1024, activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dense(units=1024, activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dense(units=512, activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dense(units=1, activation="sigmoid"),
+])
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["binary_accuracy"]
+)
+
+early_stopping = keras.callbacks.EarlyStopping(
+    patience=10,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+
+history = model.fit(
+    x=X_train, y=Y_train,
+    validation_data=(X_test, Y_test),
+    batch_size=256,
+    epochs=50,
+    callbacks=[early_stopping]
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot()
+print("Minimum validation loss: {}".format(history_df['val_loss'].min()))
 
 pred_val = model.predict(X_test)
 
-print("Score: ",  model.score(X_test, Y_test))
 print("ROC_AUC: ", roc_auc_score(Y_test, pred_val))
-print("Classification report: ", classification_report(Y_test, pred_val))
 
 pred = model.predict(x_test)
 
@@ -121,3 +165,4 @@ submission["Transported"] = pred
 
 os.makedirs('submissions/random_forests', exist_ok=True)
 submission.to_csv('submissions/random_forests/out.csv', index=False)
+plt.show()
