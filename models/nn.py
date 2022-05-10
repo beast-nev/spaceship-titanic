9c96820ef0334516925654e9448bb6415171ca27
Neville Ingram <nlingram@wpi.edu>
2022-05-10 11:00:05 -0400

    Added testing metrics

--- models/nn.py
+++ models/nn.py
@@ -1,4 +1,5 @@
 import os
+from time import asctime, localtime, time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,20 +35,31 @@ for i in float_features:
         x_train[i] = x_train[i].fillna(0)
         x_test[i] = x_test[i].fillna(0)
     else:
-        x_train[i] = SimpleImputer(strategy="mean").fit_transform(x_train[[i]])
-        x_test[i] = SimpleImputer(strategy="mean").fit_transform(x_test[[i]])
+        x_train[i] = SimpleImputer(
+            strategy="mean").fit_transform(x_train[[i]])
+        x_test[i] = SimpleImputer(
+            strategy="mean").fit_transform(x_test[[i]])
 
+x_train["most_spent"] = x_train[["RoomService",
+                                 "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].max(axis=1)
+x_train["least_spent"] = x_train[["RoomService",
+                                 "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].min(axis=1)
+x_train["std_spent"] = x_train[["RoomService",
+                                "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].std(axis=1)
+x_train["total_spent"] = x_train[["RoomService",
+                                 "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].sum(axis=1)
 
-x_train["total_spent"] = x_train["RoomService"] + \
-    x_train["FoodCourt"] + x_train["ShoppingMall"] + \
-    x_train["Spa"] + x_train["VRDeck"]
+x_test["most_spent"] = x_test[["RoomService",
+                               "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].max(axis=1)
+x_test["least_spent"] = x_test[["RoomService",
+                                "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].min(axis=1)
+x_test["std_spent"] = x_test[["RoomService",
+                              "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].std(axis=1)
+x_test["total_spent"] = x_test[["RoomService",
+                                "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]].sum(axis=1)
 
-x_test["total_spent"] = x_test["RoomService"] + \
-    x_test["FoodCourt"] + x_test["ShoppingMall"] + \
-    x_test["Spa"] + x_test["VRDeck"]
-
-# deck/num/side -> Side = P or S
-# B/0/P
+# # deck/num/side -> Side = P or S
+# # B/0/P
 x_train[['deck', 'num', 'side']] = x_train['Cabin'].str.split('/', expand=True)
 
 x_train = x_train.drop(columns=["Cabin", ])
@@ -56,17 +68,14 @@ x_test[['deck', 'num', 'side']] = x_test['Cabin'].str.split('/', expand=True)
 
 x_test = x_test.drop(columns=["Cabin", ])
 
-deckEncoder = LabelEncoder()
-x_train["deck"] = deckEncoder.fit_transform(x_train["deck"])
-x_test["deck"] = deckEncoder.fit_transform(x_test["deck"])
+x_train["deck"] = LabelEncoder().fit_transform(x_train["deck"])
+x_test["deck"] = LabelEncoder().fit_transform(x_test["deck"])
 
-numEncoder = LabelEncoder()
-x_train["num"] = deckEncoder.fit_transform(x_train["num"])
-x_test["num"] = deckEncoder.fit_transform(x_test["num"])
+x_train["num"] = LabelEncoder().fit_transform(x_train["num"])
+x_test["num"] = LabelEncoder().fit_transform(x_test["num"])
 
-portEncoder = OneHotEncoder()
-x_train["side"] = deckEncoder.fit_transform(x_train["side"])
-x_test["side"] = deckEncoder.fit_transform(x_test["side"])
+x_train["side"] = OneHotEncoder(sparse=False).fit_transform(x_train[["side"]])
+x_test["side"] = OneHotEncoder(sparse=False).fit_transform(x_test[["side"]])
 
 feature_names = x_train.columns
 print("Number of features: ", len(feature_names))
@@ -75,11 +84,43 @@ x_train['total_spent'] = x_train.apply(
     lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['total_spent'],
     axis=1
 )
+x_train['most_spent'] = x_train.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['most_spent'],
+    axis=1
+)
+x_train['least_spent'] = x_train.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['least_spent'],
+    axis=1
+)
+x_train['std_spent'] = x_train.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['std_spent'],
+    axis=1
+)
 
 x_test['total_spent'] = x_test.apply(
     lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['total_spent'],
     axis=1
 )
+x_test['most_spent'] = x_test.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['most_spent'],
+    axis=1
+)
+x_test['least_spent'] = x_test.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['least_spent'],
+    axis=1
+)
+x_test['std_spent'] = x_test.apply(
+    lambda row: 0 if row["CryoSleep"] == True or row["Age"] <= 12 else row['std_spent'],
+    axis=1
+)
+
+for i in categorical_features:
+    if i != "Cabin" and i != "CryoSleep" and i != "VIP":
+        x_train[i] = x_train[i].fillna("")
+        x_test[i] = x_test[i].fillna("")
+    elif i == "CryoSleep" or i == "VIP":
+        x_train[i] = x_train[i].fillna(False)
+        x_test[i] = x_test[i].fillna(False)
 
 for i in categorical_features:
     if i != "Cabin" and i != "Name":
@@ -97,57 +138,63 @@ feature_names = x_train.columns
 X_train, X_test, Y_train, Y_test = train_test_split(
     x_train, y_train, test_size=0.3, random_state=42, stratify=y_train)
 
-input_shape = X_train.shape[0]
-
+input_shape = X_train.shape[1]
+layer_sizes = [756, 756, 512, 256, 128, 1]
+activation_function = "relu"
+dropout_size = 0.5
 model = keras.Sequential([
+    layers.BatchNormalization(input_shape=[input_shape]),
+    layers.Dense(input_shape=[input_shape], units=layer_sizes[0], activation=activation_function,
+                 ),
     layers.BatchNormalization(),
-    layers.Dense(units=256, input_shape=[input_shape], activation="relu"),
+    layers.Dropout(dropout_size),
+    layers.Dense(units=layer_sizes[1], activation=activation_function,
+                 ),
     layers.BatchNormalization(),
-    layers.Dropout(0.5),
-    layers.Dense(units=256, activation="relu"),
-    layers.BatchNormalization(),
-    layers.Dropout(0.5),
-    layers.Dense(units=256, activation="relu"),
-    layers.BatchNormalization(),
-    layers.Dropout(0.5),
-    layers.Dense(units=256, activation="relu"),
-    layers.BatchNormalization(),
-    layers.Dropout(0.5),
-    layers.Dense(units=1, activation="sigmoid"),
+    layers.Dropout(dropout_size),
+    layers.Dense(units=layer_sizes[len(layer_sizes)-1], activation="sigmoid"),
 ])
 
+# compile model
 model.compile(
     optimizer="adam",
     loss="binary_crossentropy",
     metrics=["binary_accuracy"]
 )
 
+# set up early_stopping to avoid overfitting
 early_stopping = keras.callbacks.EarlyStopping(
-    patience=20,
+    patience=50,
     min_delta=0.001,
     restore_best_weights=True,
 )
-
+# fit the model
+batch_size = 128
+epochs = 500
 history = model.fit(
     x=X_train, y=Y_train,
     validation_data=(X_test, Y_test),
-    batch_size=128,
-    epochs=500,
-    callbacks=[early_stopping]
+    batch_size=batch_size,
+    epochs=epochs,
+    callbacks=[early_stopping],
 )
 
+# plot the trainiing loss versus validation loss to confirm no under/overfitting
 history_df = pd.DataFrame(history.history)
-# print(history_df.columns)
+print(history_df.columns)
 history_df.loc[:, ['loss', 'val_loss', ]].plot()
-print("Minimum validation loss: {}".format(history_df['val_loss'].min()))
-# print("Maximum binning accuracy: {}".format(
-#     history_df['binary_accuracy'].max()))
+print(f"Minimum validation loss: {history_df['val_loss'].min()}")
+print(f"Best val accuracy: {history_df['val_binary_accuracy'].max()}")
 
-pred_val = model.predict(X_test)
-
-print("ROC_AUC: ", roc_auc_score(Y_test, pred_val))
-
+# write results to file for evaluating model
+with open("run_results.txt", "a") as file:
+    file.write(f"\nTime: {asctime(localtime())}\n")
+    file.write(
+        f"Val binary accuracy best: {str(history_df['val_binary_accuracy'].max())}\n")
+    file.write(
+        f"With layer sizes: {layer_sizes},\nbatch_size: {batch_size},\nepochs: {epochs},\nactivation function: {activation_function},\ndropout size: {dropout_size}\n")
 pred = model.predict(x_test)
+# pred = stats.rankdata(pred)
 
 submission["Transported"] = np.round(pred)
 submission["Transported"].loc[submission["Transported"] == 1] = True
